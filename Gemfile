1419881d405cc832356583786d1010b349542c03
Julien Portalier <ysbaddaden@gmail.com>
2011-12-16 15:52:02 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1.3'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', '~> 3.1.5'
+  gem 'uglifier',   '>= 1.0.3'
+end
+
+group :test do
+  gem 'turn', '~> 0.8.3', :require => false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdown::Application.config.secret_token = '7db4d73797d22ed6fd70323563828dcb38f568c41917ddb12bc4cb02b96968640beeb2f9c381c5da59f48d2720377fb537a8193e74076f6a843b73b0720c594e'
